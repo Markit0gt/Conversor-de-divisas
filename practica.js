5610620687e0b5ef9ae90d49ba03b597b8f7315b
marco gonzalez <marcoterpin@hotmail.com>
2022-07-17 19:33:23 -0300

    primera entrega actualizacion arrays y funcion

--- practica.js
+++ practica.js
@@ -1,18 +1,19 @@
 let moneda = prompt('Escribir que moneda desea convertir (USD/EUR)')
 let dinero = prompt('ingrese la cantidad de dinero');
-let dolar = 268;
-let euro = 288;
-let resultado
+const monedas = [
+    {moneda: "dolar", value: "268"},
+    {moneda: "euro", value: "288"},
+];
+(function convertir (){
     if (moneda == "usd"){
-        resultado = dinero * dolar;
-        console.log('el resultado de USD a ARG es: ', resultado);
-        }
-    else if (moneda == "eur") {
-        resultado = dinero * euro;
-        console.log('el resultado de EUR a ARG es: ', resultado);
-        }
-    else {
-        console.log("error al selecionar la moneda")
-};
-
-
+        let resultado = dinero * monedas[0].value;
+        console.log("el resultado de dolares a argentino es: ", resultado);
+    }
+    else if (moneda == "eur"){
+        let resultado = dinero * monedas[1].value;
+        console.log("el resultado de euro a argentino es; ", resultado);
+    }
+    else{
+        alert("error al convertir")
+    }
+})();
